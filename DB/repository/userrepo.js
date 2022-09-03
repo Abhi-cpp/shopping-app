ba76db71ddc815226f15e6a957bb233e796f9264
Abhishek <abhishekkumar.ece1@bvp.edu.in>
2022-09-03 16:50:51 +0530

    async call done

--- DB/repository/userrepo.js
+++ DB/repository/userrepo.js
@@ -1,13 +1,24 @@
 const UserModel = require('../models/usermodel');
 const { response } = require('express');
+const bcrypt = require('bcrypt');
+
+
 
 module.exports = {
     register(userObj) {
         var promise = UserModel.create(userObj);
         return promise;
     },
-    login(userObj) {
-        var promise = UserModel.findOne({ email: userObj.email, password: userObj.password });
-        return promise;
+    async login(userObj) {
+        const user = await UserModel.findOne({ email: userObj.email });
+        if (user) {
+            const result = await bcrypt.compare(userObj.password, user.password);
+            if (result) {
+                return user;
+            }
+            else {
+                return null;
+            }
+        }
     }
 }
--- controller/userctrl.js
+++ controller/userctrl.js
@@ -30,32 +30,21 @@ module.exports = {
             });
     }
     ,
-    login(req, res) {
+    async login(req, res) {
         let userObj = req.body;
         console.log(userObj);
-        bcrypt.hash(userObj.password, 10)
-            .then((hash) => {
-                userObj.password = hash;
-                repo.login(userObj)
-                    .then((data) => {
-                        res.status(201).send({
-                            message: "User login Successfully",
-                            data,
-                        });
-                    })
-                    .catch((error) => {
-                        res.status(500).send({
-                            message: "Error login user",
-                            error,
-                        });
-                    })
-            })
-            .catch((e) => {
-                res.status(500).send({
-                    message: "Password was not hashed successfully",
-                    e,
-                });
+        const result = await repo.login(userObj);
+        if (result) {
+            res.status(200).send({
+                message: "User Logged In Successfully",
+                result,
             });
+        }
+        else {
+            res.status(401).send({
+                message: "Invalid Credentials",
+            });
+        }
 
     }
 }
